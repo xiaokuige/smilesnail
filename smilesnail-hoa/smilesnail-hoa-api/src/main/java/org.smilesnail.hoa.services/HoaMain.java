12ae75cd5b2ae456570e9f808601447a2613c63e
root <651023907@qq.com>
2019-12-26 22:34:07 +0800

    初始化项目smilesnail

---
+++ smilesnail-hoa/smilesnail-hoa-api/src/main/java/org.smilesnail.hoa.services/HoaMain.java
@@ -0,0 +1,15 @@
+package org.smilesnail.hoa.services;
+
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * @Description:项目启动类
+ * @author: xiaokuige
+ * @Emial: 651023907
+ * @Date： 2019/12/20 21:37
+ */
+public class HoaMain {
+    public static void main(String[] args) {
+        HttpServer httpServer = new HttpServer();
+    }
+}
---
+++ smilesnail-hoa/smilesnail-hoa-api/src/main/java/org.smilesnail.hoa.services/netty/HttpServer.java
@@ -0,0 +1,12 @@
+package org.smilesnail.hoa.services.netty;
+
+
+/**
+ * @Description:
+ * @author: xiaokuige
+ * @Emial: 651023907
+ * @Date： 2019/12/20 21:39
+ */
+public class HttpServer {
+    private Logger logger = LoggerFactory.getLogger(HttpServer.class);
+}
